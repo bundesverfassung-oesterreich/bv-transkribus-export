ef43d33dd468788c30b2658f5fdf6ccac74483c7
csae8092 <peter.andorfer@oeaw.ac.at>
2023-02-23 14:23:52 +0100

    col ID as param

--- scripts/fetch_mets.py
+++ scripts/fetch_mets.py
@@ -5,6 +5,7 @@ from transkribus_utils.transkribus_utils import ACDHTranskribusUtils
 
 user = os.environ.get('TR_USER')
 pw = os.environ.get('TR_PW')
+col_id = os.environ.get("TR_COL_ID", "188933")
 os.makedirs('./mets', exist_ok=True)
 
 transkribus_client = ACDHTranskribusUtils(
@@ -13,4 +14,4 @@ transkribus_client = ACDHTranskribusUtils(
     transkribus_base_url="https://transkribus.eu/TrpServer/rest"
 )
 
-mpr_docs = transkribus_client.collection_to_mets("188933", file_path='./mets')
+mpr_docs = transkribus_client.collection_to_mets(col_id, file_path='./mets')
