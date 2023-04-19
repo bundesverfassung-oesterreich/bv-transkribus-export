0660534d42e450b7a7be9f4fbb2ffc1e31d9b27e
cfhaak <c.f.haak@posteo.de>
2023-04-19 14:32:18 +0200

    fixed mix up in var names, that wasnt causing any errors jet but would have in the future; fixed key in dict subscription for project metadata

--- scripts/refine_tei.py
+++ scripts/refine_tei.py
@@ -21,7 +21,6 @@ project_local_baserow_dump = TMP_DIR + "project_data.json"
 PROJECT_MD = {}
 
 
-
 # # load template
 templateLoader = jinja2.FileSystemLoader(searchpath=TEMPLATE_PATH)
 templateEnv = jinja2.Environment(
@@ -43,6 +42,7 @@ class PersonMetaData:
         self.name = name
         self.role = role
         self.arche_role = arche_role
+
 
 def get_xml_doc(xml_file):
     """
@@ -141,7 +141,9 @@ def make_all_section_divs(doc):
 
 
 def remove_useless_atributes(doc: TeiReader):
-    elements = doc.any_xpath(".//*[local-name()='lb' or local-name()='p' or local-name()='head']")
+    elements = doc.any_xpath(
+        ".//*[local-name()='lb' or local-name()='p' or local-name()='head']"
+    )
     for element in elements:
         element.attrib.clear()
 
@@ -157,9 +159,7 @@ def remove_useless_elements(doc: TeiReader):
 def get_faksimile_element(doc: TeiReader, image_urls: list):
     for graphic_element in doc.any_xpath(".//tei:graphic"):
         graphic_element.attrib["url"] = image_urls.pop(0)
-    for zone_element in doc.any_xpath(
-        ".//tei:facsimile/tei:surface//tei:zone"
-    ):
+    for zone_element in doc.any_xpath(".//tei:facsimile/tei:surface//tei:zone"):
         zone_element.getparent().remove(zone_element)
     faksimile_element = doc.any_xpath(".//tei:facsimile")[0]
     return ET.tostring(faksimile_element).decode("utf-8")
@@ -188,7 +188,7 @@ def create_new_xml_data(
     }
     xml_data = template.render(context)
     doc = TeiReader(xml_data)
-    doc.tree_to_file(os.path.join(TEI_DIR, doc_metadata["bv_id"]+".xml"))
+    doc.tree_to_file(os.path.join(TEI_DIR, doc_metadata["bv_id"] + ".xml"))
 
 
 def return_image_urls(mets_doc):
@@ -223,6 +223,7 @@ def return_col_id_from_mets_doc(doc: TeiReader):
 
 def fetch_metadata_dump(url, local_filepath):
     import requests
+
     results = requests.get(url)
     json_result = results.json()
     with open(local_filepath, "w") as outfile:
@@ -234,7 +235,7 @@ def load_metadata_from_dump():
     json_data = None
     if not os.path.exists(doc_local_baserow_dump):
         _ = fetch_metadata_dump(
-            url=project_base_row_dump_url, local_filepath=project_local_baserow_dump
+            url=doc_base_row_dump_url, local_filepath=doc_local_baserow_dump
         )
         json_data = fetch_metadata_dump(
             url=doc_base_row_dump_url, local_filepath=doc_local_baserow_dump
@@ -248,7 +249,9 @@ def load_metadata_from_dump():
         transcribus_col_id = mets_dict["transkribus_col_id"]
         if transcribus_col_id not in meta_data_objs_by_transkribus_id:
             meta_data_objs_by_transkribus_id[transcribus_col_id] = {}
-        meta_data_objs_by_transkribus_id[transcribus_col_id][mets_dict["transkribus_doc_id"]] = mets_dict
+        meta_data_objs_by_transkribus_id[transcribus_col_id][
+            mets_dict["transkribus_doc_id"]
+        ] = mets_dict
     return meta_data_objs_by_transkribus_id
 
 
@@ -269,7 +272,9 @@ def process_all_files():
                 # # organize data yet missing in the final doc
                 transkribus_doc_id = return_transkribus_doc_id(xml_file_path)
                 if transkribus_doc_id not in collection_metadata:
-                    print(f"No metadata found for transkribus-doc-id '{transkribus_doc_id}'.")            
+                    print(
+                        f"No metadata found for transkribus-doc-id '{transkribus_doc_id}'."
+                    )
                 else:
                     doc_metadata = collection_metadata[transkribus_doc_id]
                     mets_doc = return_mets_doc(
@@ -284,13 +289,14 @@ def process_all_files():
 if __name__ == "__main__":
     # # the following is needed, cause you never know if this stuff ist there.
     if os.path.isfile(project_local_baserow_dump):
-        with open(project_local_baserow_dump, "r", encoding="utf-8") as json_metadata_file:
+        with open(
+            project_local_baserow_dump, "r", encoding="utf-8"
+        ) as json_metadata_file:
             PROJECT_MD = json.load(json_metadata_file)["1"]
     else:
         PROJECT_MD = fetch_metadata_dump(
-            url=doc_base_row_dump_url,
-            local_filepath=doc_local_baserow_dump
-        )
+            url=project_base_row_dump_url, local_filepath=project_local_baserow_dump
+        )["1"]
     # # clear directory for new export
     shutil.rmtree(TEI_DIR, ignore_errors=True)
     os.makedirs(TEI_DIR, exist_ok=True)
@@ -301,4 +307,3 @@ if __name__ == "__main__":
         print(
             f"\n{file_rename_errors} file(s) couldn’t be renamed since title wasn’t found in xml\n"
         )
-    os.remove(doc_local_baserow_dump)
